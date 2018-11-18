aa7ac5e75b9ff06102b40d9af0f566398666895e
bodecobra <44956562+bodecobra@users.noreply.github.com>
2018-11-18 09:19:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,17 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const prefix = ['-']
 
 client.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`Credit Farmm - Script By : Bode `);
-   console.log(`----------------`);
-   console.log(`Loadinng`);
-   console.log(`Loadinng.`);
-   console.log(`Loadinng..`);
-   console.log(`Loadinng...`);
-   console.log(`This Bots Online ' `);
-   console.log(`----------------`);
+  console.log('======================================')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('=======================================')
 });
-
 
 client.on('message', message => {
     if(message.content === '-راتب'){
@@ -29,8 +27,41 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.content === '-ريب'){
-        message.channel.send('#credits')
+        message.channel.send('#rep <@286088294234718209>')
     }
+});
+
+client.on('message', message => {
+if (message.content === "سبام" + "<@" + `${client.user.id}` + ">") {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`سبااااام Spam , Credit ${x}`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', '.');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
 });
 
 client2.on('message', message => {
@@ -47,18 +78,16 @@ client2.on('message', message => {
 
 client2.on('message', message => {
     if(message.content === '-ريب'){
-        message.channel.send('#credits')
+        message.channel.send('#rep <@286088294234718209>')
     }
 });
 
-
-
-client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
+client2.on('message', message => {
+if (message.content === "سبام" + "<@" + `${client.user.id}` + ">") {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Bode , Farm Credit By Bode, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`سبااااام Spam , Credit ${x}`)
           .then(m => {
             count++;
           })
@@ -67,22 +96,25 @@ if (message.content === '!spam') {
       }
 });
 
-client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Bode , Farm Credit By Bode, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
+client2.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', '.');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
 });
 
 
-
-
-client.login(process.env.TOKEN);// لا تغير فيها شيء
-client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
